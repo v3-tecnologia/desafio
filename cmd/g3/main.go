415bb6467c191ad52c894e9c8125bd5f8a528a08
charmingruby <mrrubycanal3105@gmail.com>
2024-08-20 18:44:34 -0300

    feat: environment config

--- cmd/g3/main.go
+++ cmd/g3/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/charmingruby/g3/config"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("hello world")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := godotenv.Load(); err != nil {
+		slog.Warn("CONFIGURATION: .env file not found")
+	}
+
+	_, err := config.NewConfig()
+	if err != nil {
+		slog.Error(fmt.Sprintf("CONFIGURATION: %s", err.Error()))
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/charmingruby/g3
 
 go 1.22.3
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/joho/godotenv v1.5.1
+)
