28a1c92f6ada23f23658b703b681a2eafa7c9285
charmingruby <mrrubycanal3105@gmail.com>
2024-08-21 11:19:35 -0300

    feat: create gyroscope endpoint

--- internal/telemetry/transport/rest/endpoint/v1/create_gyroscope_endpoint.go
+++ internal/telemetry/transport/rest/endpoint/v1/create_gyroscope_endpoint.go
@@ -1,11 +1,45 @@
 package v1
 
 import (
-	"net/http"
-
+	"github.com/charmingruby/g3/internal/common/api/api_rest"
+	"github.com/charmingruby/g3/internal/common/custom_err"
+	"github.com/charmingruby/g3/internal/telemetry/domain/dto"
+	"github.com/charmingruby/g3/internal/telemetry/transport/rest/endpoint/v1/v1_presenter"
 	"github.com/gin-gonic/gin"
 )
 
+type CreateGyroscopeRequest struct {
+	XPosition float64 `json:"x" binding:"required"`
+	YPosition float64 `json:"y" binding:"required"`
+	ZPosition float64 `json:"z" binding:"required"`
+}
+
 func (h *Handler) createGyroscopeEndpoint(c *gin.Context) {
-	c.JSON(http.StatusOK, "ok")
+	var req CreateGyroscopeRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		api_rest.NewPayloadError(c, err)
+		return
+	}
+
+	dto := dto.CreateGyroscopeInputDTO{
+		XPosition: req.XPosition,
+		YPosition: req.YPosition,
+		ZPosition: req.ZPosition,
+	}
+
+	output, err := h.telemetryService.CreateGyroscopeUseCase(dto)
+	if err != nil {
+		validationErr, ok := err.(*custom_err.ErrValidation)
+		if ok {
+			api_rest.NewEntityError(c, validationErr)
+			return
+		}
+
+		api_rest.NewInternalServerError(c, err)
+		return
+	}
+
+	mappedGyroscope := v1_presenter.DomainGyroscopeToHTTP(output.Gyroscope)
+
+	api_rest.NewCreatedResponse(c, "gyroscope", mappedGyroscope)
 }
---
+++ internal/telemetry/transport/rest/endpoint/v1/v1_presenter/gyroscope_presenter.go
@@ -0,0 +1,26 @@
+package v1_presenter
+
+import (
+	"time"
+
+	"github.com/charmingruby/g3/internal/telemetry/domain/entity"
+)
+
+func DomainGyroscopeToHTTP(gyroscope entity.Gyroscope) HTTPGyroscope {
+	return HTTPGyroscope{
+		ID:        gyroscope.ID,
+		XPosition: gyroscope.XPosition,
+		YPosition: gyroscope.YPosition,
+		ZPosition: gyroscope.ZPosition,
+		CreatedAt: gyroscope.CreatedAt,
+	}
+
+}
+
+type HTTPGyroscope struct {
+	ID        string    `json:"id"`
+	XPosition float64   `json:"x"`
+	YPosition float64   `json:"y"`
+	ZPosition float64   `json:"z"`
+	CreatedAt time.Time `json:"created_at"`
+}
