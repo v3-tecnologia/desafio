bda215dc85fc1411be4879db22a3fed76a7dd8fd
charmingruby <mrrubycanal3105@gmail.com>
2024-08-21 08:42:00 -0300

    feat: create all the use case, but not implemented

---
+++ internal/telemetry/domain/usecase/create_gyroscope_usecase.go
@@ -0,0 +1,7 @@
+package usecase
+
+import "github.com/charmingruby/g3/internal/telemetry/domain/dto"
+
+func (r *TelemetryUseCaseRegistry) CreateGyroscopeUseCase(input dto.CreateGyroscopeInputDTO) (dto.CreateGyroscopeOutputDTO, error) {
+	return dto.CreateGyroscopeOutputDTO{}, nil
+}
---
+++ internal/telemetry/domain/usecase/create_photo_usecase.go
@@ -0,0 +1,7 @@
+package usecase
+
+import "github.com/charmingruby/g3/internal/telemetry/domain/dto"
+
+func (r *TelemetryUseCaseRegistry) CreatePhotoUseCase(input dto.CreatePhotoInputDTO) (dto.CreatePhotoOutputDTO, error) {
+	return dto.CreatePhotoOutputDTO{}, nil
+}
