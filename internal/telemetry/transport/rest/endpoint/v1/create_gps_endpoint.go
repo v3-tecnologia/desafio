c5bc4dff7c2b2b09abf4e85d50cfe04bf84d33ee
charmingruby <mrrubycanal3105@gmail.com>
2024-08-21 11:09:57 -0300

    feat: create gps endpoint

--- internal/telemetry/transport/rest/endpoint/v1/create_gps_endpoint.go
+++ internal/telemetry/transport/rest/endpoint/v1/create_gps_endpoint.go
@@ -1,11 +1,43 @@
 package v1
 
 import (
-	"net/http"
-
+	"github.com/charmingruby/g3/internal/common/api/api_rest"
+	"github.com/charmingruby/g3/internal/common/custom_err"
+	"github.com/charmingruby/g3/internal/telemetry/domain/dto"
+	"github.com/charmingruby/g3/internal/telemetry/transport/rest/endpoint/v1/v1_presenter"
 	"github.com/gin-gonic/gin"
 )
 
+type CreateGPSRequest struct {
+	Latitude  float64 `json:"latitude" binding:"required"`
+	Longitude float64 `json:"longitude" binding:"required"`
+}
+
 func (h *Handler) createGPSEndpoint(c *gin.Context) {
-	c.JSON(http.StatusOK, "ok")
+	var req CreateGPSRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		api_rest.NewPayloadError(c, err)
+		return
+	}
+
+	dto := dto.CreateGPSInputDTO{
+		Latitude:  req.Latitude,
+		Longitude: req.Longitude,
+	}
+
+	output, err := h.telemetryService.CreateGPSUseCase(dto)
+	if err != nil {
+		validationErr, ok := err.(*custom_err.ErrValidation)
+		if ok {
+			api_rest.NewEntityError(c, validationErr)
+			return
+		}
+
+		api_rest.NewInternalServerError(c, err)
+		return
+	}
+
+	mappedGPS := v1_presenter.DomainGPSToHTTP(output.GPS)
+
+	api_rest.NewCreatedResponse(c, "gps", mappedGPS)
 }
---
+++ internal/telemetry/transport/rest/endpoint/v1/v1_presenter/gps_presenter.go
@@ -0,0 +1,24 @@
+package v1_presenter
+
+import (
+	"time"
+
+	"github.com/charmingruby/g3/internal/telemetry/domain/entity"
+)
+
+func DomainGPSToHTTP(gps entity.GPS) HTTPGPS {
+	return HTTPGPS{
+		ID:        gps.ID,
+		Latitude:  gps.Latitude,
+		Longitude: gps.Longitude,
+		CreatedAt: gps.CreatedAt,
+	}
+
+}
+
+type HTTPGPS struct {
+	ID        string    `json:"id"`
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	CreatedAt time.Time `json:"created_at"`
+}
