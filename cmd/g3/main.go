615c88882f5c0a371d80ea9b2e90b68cbd528da2
charmingruby <mrrubycanal3105@gmail.com>
2024-08-21 09:47:36 -0300

    feat: rest server setup

--- cmd/g3/main.go
+++ cmd/g3/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/charmingruby/g3/config"
+	"github.com/charmingruby/g3/internal/common/api/api_rest"
 	"github.com/charmingruby/g3/pkg/postgres"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -29,4 +37,35 @@ func main() {
 		slog.Error(fmt.Sprintf("DATABASE: %s", err.Error()))
 		os.Exit(1)
 	}
+
+	router := gin.Default()
+	api_rest.SetupCORS(router)
+
+	// init dependencies
+
+	server := api_rest.NewServer(router, "3000")
+
+	go func() {
+		if err := server.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error(fmt.Sprintf("REST SERVER: %s", err.Error()))
+			os.Exit(1)
+		}
+	}()
+
+	// Graceful shutdown
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	<-sc
+
+	slog.Info("HTTP Server interruption received!")
+
+	ctx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Server.Shutdown(ctx); err != nil {
+		slog.Error(fmt.Sprintf("GRACEFUL SHUTDOWN REST SERVER: %s", err.Error()))
+		os.Exit(1)
+	}
+
+	slog.Info("Gracefully shutdown!")
 }
---
+++ internal/common/api/api_rest/cors.go
@@ -0,0 +1,14 @@
+package api_rest
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupCORS(router *gin.Engine) {
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowHeaders:    []string{"Origin", "Accept", "Content-Type", "Authorization", "User-Agent"},
+		ExposeHeaders:   []string{"Content-Length"},
+	}))
+}
