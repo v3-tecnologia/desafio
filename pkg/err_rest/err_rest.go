10515fede5a18ff965977a1672aa2727a3b9f4e1
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-08-24 17:24:24 -0300

    feat: add errRest para padronizacao dos errs

--- pkg/err_rest/err_rest.go
+++ pkg/err_rest/err_rest.go
@@ -1,1 +1,29 @@
 package err_rest
+
+import "net/http"
+
+type ErrRest struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Err  string `json:"err"`
+}
+
+func (e *ErrRest) Error() string {
+	return e.Msg
+}
+
+func NewBadRequestErr(msg string) *ErrRest {
+	return &ErrRest{
+		Code: http.StatusBadRequest,
+		Msg:  msg,
+		Err:  "Err_status_bad_request",
+	}
+}
+
+func NewInternalServerError(msg string) *ErrRest {
+	return &ErrRest{
+		Code: http.StatusInternalServerError,
+		Msg:  msg,
+		Err:  "Err_internal_server_error",
+	}
+}
