2247099b6e26a4d73ddc36d37f19fd35e3548923
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-08-24 17:43:51 -0300

    feat: add funcao para iniciar postrgres

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ThalesMonteir0/desafio
 
 go 1.22.6
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- pkg/database/postgresql/postgresql.go
+++ pkg/database/postgresql/postgresql.go
@@ -1,1 +1,33 @@
 package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+const (
+	HOST        = "HOST"
+	PORT        = "PORT"
+	USER_DB     = "USER_DB"
+	PASSWORD_DB = "PASSWORD_DB"
+	DB_NAME     = "DB_NAME"
+)
+
+func OpenConnDB() (*sql.DB, error) {
+	host := os.Getenv(HOST)
+	port := os.Getenv(PORT)
+	userDB := os.Getenv(USER_DB)
+	passwordDB := os.Getenv(PASSWORD_DB)
+	dbName := os.Getenv(DB_NAME)
+	dsnString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, userDB, passwordDB, dbName)
+
+	db, err := sql.Open("postgres", dsnString)
+	if err != nil {
+		panic("unable connect to db")
+	}
+
+	err = db.Ping()
+
+	return db, err
+}
