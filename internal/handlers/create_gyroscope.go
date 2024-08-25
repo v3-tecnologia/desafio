432e468891900cef21280edaa216d96b3f942b4b
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-08-24 21:38:17 -0300

    feat: add campo mac address ao dominio de gyroscope, photo e gps

--- internal/handlers/create_gyroscope.go
+++ internal/handlers/create_gyroscope.go
@@ -1,8 +1,58 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/ThalesMonteir0/desafio/internal/domain"
+	"github.com/ThalesMonteir0/desafio/internal/handlers/models/request"
+	"github.com/ThalesMonteir0/desafio/pkg/err_rest"
+	"io"
+	"net/http"
+)
 
-func (t telemetryHandler) CreateGyroscope(writer http.ResponseWriter, request *http.Request) {
-	//TODO implement me
-	panic("implement me")
+func (t telemetryHandler) CreateGyroscope(w http.ResponseWriter, r *http.Request) {
+	var gyroscopeRequest request.GyroscopeRequest
+	var errRest *err_rest.ErrRest
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		errRest = err_rest.NewBadRequestErr("unable read body")
+		w.WriteHeader(errRest.Code)
+		return
+	}
+
+	if err = json.Unmarshal(body, &gyroscopeRequest); err != nil {
+		errRest = err_rest.NewUnprocessableEntityError("unable to process the request because it contains invalid data")
+		w.WriteHeader(errRest.Code)
+		return
+	}
+
+	if errRest = validateGyroscopeBody(gyroscopeRequest); err != nil {
+		w.WriteHeader(errRest.Code)
+		return
+	}
+
+	gyroscopeDomain := domain.GyroscopeDomain{
+		X:          gyroscopeRequest.X,
+		Y:          gyroscopeRequest.Y,
+		Z:          gyroscopeRequest.Z,
+		MacAddress: gyroscopeRequest.MacAddress,
+	}
+
+	if errRest = t.service.CreateGyroscopeService(gyroscopeDomain); err != nil {
+		w.WriteHeader(errRest.Code)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func validateGyroscopeBody(gyroscopeRequest request.GyroscopeRequest) *err_rest.ErrRest {
+	if gyroscopeRequest.MacAddress == "" {
+		return err_rest.NewBadRequestErr("field mac_address is required")
+	}
+
+	if gyroscopeRequest.CollectionDate.IsZero() {
+		return err_rest.NewBadRequestErr("field collection_date is required")
+	}
+
+	return nil
 }
--- internal/handlers/models/request/telemetry.go
+++ internal/handlers/models/request/telemetry.go
@@ -1,1 +1,22 @@
 package request
+
+import "time"
+
+type GyroscopeRequest struct {
+	X              float64   `json:"x"`
+	Y              float64   `json:"y"`
+	Z              float64   `json:"z"`
+	MacAddress     string    `json:"mac_address"`
+	CollectionDate time.Time `json:"collection_date"`
+}
+type GpsRequest struct {
+	Latitude       float64   `json:"latitude"`
+	Longitude      float64   `json:"longitude"`
+	MacAddress     string    `json:"mac_address"`
+	CollectionDate time.Time `json:"collection_date"`
+}
+type PhotoRequest struct {
+	Url            string    `json:"url"`
+	MacAddress     string    `json:"mac_address"`
+	CollectionDate time.Time `json:"collection_date"`
+}
