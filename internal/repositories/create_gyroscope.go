4cef124e70c8a0a002ded7a4af30be3c537d6bb7
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-08-24 22:13:33 -0300

    feat: implementando createGyroscope repo

--- internal/repositories/create_gyroscope.go
+++ internal/repositories/create_gyroscope.go
@@ -2,10 +2,29 @@ package repositories
 
 import (
 	"github.com/ThalesMonteir0/desafio/internal/domain"
+	"github.com/ThalesMonteir0/desafio/internal/repositories/convert"
 	"github.com/ThalesMonteir0/desafio/pkg/err_rest"
 )
 
 func (t telemetryRepository) CreateGyroscope(gyroscopeDomain domain.GyroscopeDomain) *err_rest.ErrRest {
-	//TODO implement me
-	panic("implement me")
+	entity := convert.ConvertGyroscopeDomainToEntity(gyroscopeDomain)
+	sql := `INSERT INTO public.gyroscopes(
+		device_id, x, y, z, collection_date)
+		VALUES ($1, $2, $3, $4, $5);`
+
+	result, err := t.db.Exec(sql, entity.DeviceID, entity.X, entity.Y, entity.Z, entity.CollectionDate)
+	if err != nil {
+		return err_rest.NewInternalServerError("unable create gyroscope")
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err_rest.NewInternalServerError("unable create gyroscope")
+	}
+
+	if rowsAffected != 0 {
+		return err_rest.NewInternalServerError("unable create gyroscope")
+	}
+
+	return nil
 }
