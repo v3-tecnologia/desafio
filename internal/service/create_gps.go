826f907509de6fa1fe582b3a8fff3ea39e60df4a
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-08-24 22:45:46 -0300

    feat: implementando service de create gps

--- internal/service/create_gps.go
+++ internal/service/create_gps.go
@@ -6,6 +6,16 @@ import (
 )
 
 func (t telemetryService) CreateGpsService(gpsDomain domain.GpsDomain) *err_rest.ErrRest {
-	//TODO implement me
-	panic("implement me")
+	device, err := t.findDevice(gpsDomain.MacAddress)
+	if err != nil {
+		return err
+	}
+
+	gpsDomain.DeviceID = device.ID
+
+	if restErr := t.repository.CreateGps(gpsDomain); restErr != nil {
+		return restErr
+	}
+
+	return nil
 }
