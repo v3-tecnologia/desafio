a7745b0b39422aabbcea923c1d07a50c07da7185
FernandoHDFAraujo <fernandodelfiore@gmail.com>
2024-08-24 22:18:08 -0300

    feat: add route and handler for gyroscope

---
+++ internal/api/handlers/gyroscope.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"net/http"
+	"time"
+	"v3/pkg/httpcore"
+	"v3/pkg/models"
+	"v3/pkg/utils"
+)
+
+func (tc *ApiController) CreateGyroscope(w http.ResponseWriter, r *http.Request) (any, int) {
+	newGyro, err := httpcore.DecodeBody[models.Gyroscope](w, r)
+	if err != nil {
+		return httpcore.ErrBadRequest.With(err), http.StatusBadRequest
+	}
+
+	g, err := models.NewGyroscope(
+		&models.DeviceData{
+			MAC:       utils.GenerateMac(),
+			Timestamp: time.Now(),
+		},
+		newGyro.X,
+		newGyro.Y,
+		newGyro.Z,
+	)
+
+	if err != nil {
+		return httpcore.ErrBadRequest.With(err), http.StatusBadRequest
+	}
+
+	tc.db = append(tc.db, g)
+
+	return g, http.StatusCreated
+}
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"v3/internal/api/handlers"
+	"v3/pkg/httpcore"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func applyRoutes(router chi.Router, ctl *handlers.ApiController) {
+	router.Post("/telemetry/gyroscope", httpcore.Handle(ctl.CreateGyroscope))
 
 }
