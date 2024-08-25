e1e7673c01b43615d5b6e5cf2f959a275ad1f09a
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-08-24 23:28:38 -0300

    feat: injetando dependencias a partir do main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/ThalesMonteir0/desafio/internal/handlers"
+	"github.com/ThalesMonteir0/desafio/internal/repositories"
+	"github.com/ThalesMonteir0/desafio/internal/routers"
+	"github.com/ThalesMonteir0/desafio/internal/service"
+	"github.com/ThalesMonteir0/desafio/pkg/database/postgresql"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -10,4 +17,25 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	server := http.NewServeMux()
+
+	db, err := postgresql.OpenConnDB()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	telemetryHandler := initDependencies(db)
+
+	routers.TelemetryRoutes(server, telemetryHandler)
+
+	if err = http.ListenAndServe(":5000", server); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func initDependencies(db *sql.DB) handlers.TelemetryHandler {
+	telemetryRepository := repositories.NewTelemetryRepository(db)
+	telemetryService := service.NewTelemetryService(telemetryRepository)
+	return handlers.NewTelemetryHandler(telemetryService)
 }
