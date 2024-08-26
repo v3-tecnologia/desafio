e814989560f7281b2bb86e7f8cc850c113a2ec4a
FernandoHDFAraujo <fernandodelfiore@gmail.com>
2024-08-25 23:24:21 -0300

    chore: minor cleaning on gyroscope struct

--- pkg/models/gyroscope.go
+++ pkg/models/gyroscope.go
@@ -2,14 +2,13 @@ package models
 
 import (
 	"errors"
-	"fmt"
 )
 
 type Gyroscope struct {
-	*DeviceData `json:"deviceData"`
-	X           *float64 `json:"x"`
-	Y           *float64 `json:"y"`
-	Z           *float64 `json:"z"`
+	DeviceData *DeviceData `json:"deviceData"`
+	X          *float64    `json:"x"`
+	Y          *float64    `json:"y"`
+	Z          *float64    `json:"z"`
 }
 
 func NewGyroscope(d *DeviceData, x, y, z *float64) (*Gyroscope, error) {
@@ -18,17 +17,7 @@ func NewGyroscope(d *DeviceData, x, y, z *float64) (*Gyroscope, error) {
 	}
 
 	if x == nil || y == nil || z == nil {
-		var nilVars []string
-		if x == nil {
-			nilVars = append(nilVars, "X")
-		}
-		if y == nil {
-			nilVars = append(nilVars, "Y")
-		}
-		if z == nil {
-			nilVars = append(nilVars, "Z")
-		}
-		return nil, fmt.Errorf("%v cannot be nil", nilVars)
+		return nil, errors.New("gyroscope data cannot have nil values")
 	}
 
 	return &Gyroscope{
