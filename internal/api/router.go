fa4b4b1e6d9fd17cbfa266ef279346496401f476
FernandoHDFAraujo <fernandodelfiore@gmail.com>
2024-08-26 11:18:30 -0300

    feat: added rate limiting and throttling

--- internal/api/router.go
+++ internal/api/router.go
@@ -10,6 +10,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/go-chi/httprate"
 )
 
 func InitService() http.Handler {
@@ -18,9 +19,17 @@ func InitService() http.Handler {
 
 	router.Use(cors.New(httpcore.DefaultCorsOptions).Handler)
 
-	router.Use(middleware.Timeout(200 * time.Second))
+	//Timeout longo só pra facilitar debugging
+	router.Use(middleware.Timeout(400 * time.Second))
 
 	router.Use(middleware.Recoverer)
+
+	//Permitindo até 50 requests por minuto
+	//antes do throttling colocar os próximos requests na fila
+	router.Use(middleware.Throttle(50))
+
+	//Cada IP pode fazer 10 requests por minuto
+	router.Use(httprate.LimitByIP(10, 1*time.Minute))
 
 	router.Use(httpcore.LoggerMiddleware)
 
