d85a8514a1cef058f83a647688c7ec01fb3790f8
Pedro Leão <pedrohenriquealeo@gmail.com>
2024-08-26 12:11:26 -0300

    finish basic handlers and data validation

--- desafio/handlers/handlers.go
+++ desafio/handlers/handlers.go
@@ -5,26 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type GyroscopeRequest struct {
-	Mac           string  `json:"macAddr"`
-	X             float64 `json:"x"`
-	Y             float64 `json:"y"`
-	Z             float64 `json:"z"`
-	UnixtimeStamp int64   `json:"timeStamp"`
+	Mac           string  `json:"macAddr" validate:"required,mac"`
+	X             float64 `json:"x" validate:"required,number"`
+	Y             float64 `json:"y" validate:"required,number"`
+	Z             float64 `json:"z" validate:"required,number"`
+	UnixtimeStamp int64   `json:"timeStamp" validate:"required,number"`
 }
 
 type GpsRequest struct {
-	Mac           string  `json:"macAddr"`
-	Lat           float64 `json:"latitude"`
-	Lon           float64 `json:"longitude"`
-	UnixtimeStamp int64   `json:"timeStamp"`
+	Mac           string `json:"macAddr" validate:"required,mac"`
+	Lat           string `json:"latitude" validate:"required,latitude"`
+	Lon           string `json:"longitude" validate:"required,longitude"`
+	UnixtimeStamp int64  `json:"timeStamp" validate:"required,number"`
 }
 
 type PhotoRequest struct {
-	Mac           string `json:"macAddr"`
-	UnixtimeStamp int64  `json:"timeStamp"`
+	Mac           string `json:"macAddr" validate:"required,mac"`
+	ImageBase64   string `json:"image" validate:"required"`
+	UnixtimeStamp int64  `json:"timeStamp" validate:"required,number"`
 }
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +37,8 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 func GyroscopeHandler(w http.ResponseWriter, r *http.Request) {
 	var gyroscopeRequest GyroscopeRequest
 
+	validate := validator.New(validator.WithRequiredStructEnabled())
+
 	err := json.NewDecoder(r.Body).Decode(&gyroscopeRequest)
 	if err != nil {
 		log.Println(err.Error())
@@ -41,13 +46,20 @@ func GyroscopeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(gyroscopeRequest)
+	if err := validate.Struct(gyroscopeRequest); err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 	return
 }
 
 func GpsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("gps")
 	var gpsRequest GpsRequest
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
 
 	err := json.NewDecoder(r.Body).Decode(&gpsRequest)
 	if err != nil {
@@ -56,10 +68,34 @@ func GpsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(gpsRequest)
+	if err := validate.Struct(gpsRequest); err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 	return
 }
 
 func PhotoHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("photo")
+	var photoRequest PhotoRequest
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+
+	err := json.NewDecoder(r.Body).Decode(&photoRequest)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := validate.Struct(photoRequest); err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	return
 }
