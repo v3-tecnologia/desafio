2fa0f6a091380d6e7d6f8538582f9add67fe2686
valdson <vhpgoncalves@gmail.com>
2024-08-28 10:40:43 -0300

    Implemeentação das funções de inserção dos dados no banco

--- repository/repository.go
+++ repository/repository.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"database/sql"
 	"fmt"
+	"github/desafio/models"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -15,6 +17,18 @@ const (
 	password = "desafio"
 	dbname   = "postgres"
 )
+
+type Repository struct {
+	db *sql.DB
+}
+
+func NewRepository(client *sql.DB) (Repository, error) {
+	db, err := DBConnection()
+	if err != nil {
+		return Repository{}, err
+	}
+	return Repository{db: db}, nil
+}
 
 func DBConnection() (*sql.DB, error) {
 	host := os.Getenv("host")
@@ -38,4 +52,76 @@ func DBConnection() (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func (r *Repository) InsertGyroscopeData(data models.Gyroscope) error {
+
+	insertQuery := `INSERT INTO public.gyroscope_data
+	(macaddress, xcoord, ycoord, zcoord, data_timestamp, created)
+	VALUES($1, $2, $3, $4, $5, $6);`
+
+	sqlStatement, err := r.db.Prepare(insertQuery)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+	defer sqlStatement.Close()
+
+	_, err = sqlStatement.Exec(data.MacAddress, data.X, data.Y, data.Z, data.Timestamp, time.Now().Unix())
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+
+func (r *Repository) InsertGpsData(data models.GPS) error {
+
+	insertQuery := `INSERT INTO public.gps_data
+	(macaddress, latitude, longitude, data_timestamp, created)
+	VALUES($1, $2, $3, $4, $5);`
+
+	sqlStatement, err := r.db.Prepare(insertQuery)
+	if err != nil {
+		println("gps insert error: ")
+		log.Println(err.Error())
+		return err
+	}
+	defer sqlStatement.Close()
+
+	_, err = sqlStatement.Exec(data.MacAddress, data.Latitude, data.Longitude, data.Timestamp, time.Now().Unix())
+	if err != nil {
+		println("gps insert error: ")
+		log.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+
+func (r *Repository) InsertPhotoData(data models.Photo) error {
+
+	insertQuery := `INSERT INTO public.photo
+	(macaddress, photo, data_timestamp, created)
+	VALUES($1, decode($2, 'base64'), $3, $4);`
+
+	sqlStatement, err := r.db.Prepare(insertQuery)
+	if err != nil {
+		println("photo insert error: ")
+		log.Println(err.Error())
+		return err
+	}
+	defer sqlStatement.Close()
+
+	_, err = sqlStatement.Exec(data.MacAddress, data.Photo, data.Timestamp, time.Now().Unix())
+	if err != nil {
+		println("photo insert error: ")
+		log.Println(err.Error())
+		return err
+	}
+
+	return nil
 }
