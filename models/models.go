8beab2d4f16bedc0a27839ef81d8bc30ced3fbef
valdson <vhpgoncalves@gmail.com>
2024-08-28 15:39:40 -0300

    Ajustes nos validate dos models

--- models/models.go
+++ models/models.go
@@ -3,16 +3,16 @@ package models
 type Gyroscope struct {
 	MacAddress string  `json:"macAddress" validate:"mac,required"`
 	Timestamp  int     `json:"timestamp" validate:"required,number"`
-	X          float64 `json:"x" validate:"required"`
-	Y          float64 `json:"y" validate:"required"`
-	Z          float64 `json:"z" validate:"required"`
+	X          float64 `json:"x" validate:"required,number"`
+	Y          float64 `json:"y" validate:"required,number"`
+	Z          float64 `json:"z" validate:"required,number"`
 }
 
 type GPS struct {
 	MacAddress string `json:"macAddress" validate:"mac,required"`
 	Timestamp  int    `json:"timestamp" validate:"required,number"`
-	Latitude   string `json:"latitude" validate:"required"`
-	Longitude  string `json:"longitude" validate:"required"`
+	Latitude   string `json:"latitude" validate:"required,latitude"`
+	Longitude  string `json:"longitude" validate:"required,longitude"`
 }
 
 type Photo struct {
