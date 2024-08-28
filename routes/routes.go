0b24349c4a907ab71bce23cd03ad5474dbc2fa09
valdson <vhpgoncalves@gmail.com>
2024-08-28 16:08:04 -0300

    Adicionando o tratamento do erro de CORS em routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,17 +5,30 @@ import (
 	"github/desafio/repository"
 	"github/desafio/service"
 	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func InitializeRoutes(router *mux.Router) {
+
 	repository, err := repository.NewRepository()
-	if err != nil{
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 	handle := handlers.NewHandle(service.NewService(repository))
+	health(router)
 	router.HandleFunc("/telemetry/gyroscope", handle.GyroscopeData).Methods("POST")
 	router.HandleFunc("/telemetry/gps", handle.GPSData).Methods("POST")
 	router.HandleFunc("/telemetry/photo", handle.PhotoData).Methods("POST")
+	mux.CORSMethodMiddleware(router)
+}
+
+func health(router *mux.Router) {
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(":)"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
 }
